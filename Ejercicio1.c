dc43d4314033f67b307a410b0176c051031d96f2
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-11-29 16:07:19 -0500

    Lineas de codigo para imprimir las matrices A y B con ciclo For

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -35,6 +35,41 @@ int main(int argc, char const *argv[])
             scanf("%d", &MatrizB[i][j]);
         }
     }
+     //Impresion Matriz A
+     printf("Matriz A:\n");
+    for (int i = 0; i < fA; i++)
+    {
+        for (int j = 0; j < cA; j++)
+        {
+            printf("%d\t", MatrizA[i][j]);
+        }
+        printf("\n\n");
+    }
+
+    //Impresion Matriz B
+    printf("\nMatriz B:\n");
+    for (int i = 0; i < fB; i++)
+    {
+        for (int j = 0; j < cB; j++)
+        {
+            printf("%d\t", MatrizB[i][j]);
+        }
+        printf("\n\n");
+    }
+
+    int MatrizC[fC][cC];
+    for (int i = 0; i < fC; i++)
+    {
+        for (int j = 0; j < cC; j++)
+        {
+            MatrizC[fC][cC]=MatrizA[fA][cA]+MatrizB[fB][cB];
+            printf("%d\t", MatrizC[i][j]);
+        }
+        printf("\n\n");
+    }
+
+
+    
     //printf("%d", MatrizB[fB][cB]);
      //printf("%d", MatrizA[fA][cA]);
 
