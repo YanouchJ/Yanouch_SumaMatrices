26742e331a0fa31bb8315ec1b9d13f4311f24a47
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-11-29 16:27:19 -0500

    Estructura para sumar las matrices A y B e impresion de la Matriz C

--- Ejercicio1.c
+++ Ejercicio1.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(int argc, char const *argv[])
 {
-
+    srand(time(NULL));
     int fA, cA, fB, cB, fC, cC;
+    // Dimension de la matriz A
     printf("MATRIZ A\n");
     printf("Ingrese el numero de filas: ");
     scanf("%d", &fA);
@@ -11,15 +13,15 @@ int main(int argc, char const *argv[])
     scanf("%d", &cA);
     int MatrizA[fA][cA];
 
+    // Valores para la matriz A
     for (int i = 0; i < fA; i++)
     {
         for (int j = 0; j < cA; j++)
         {
-            printf("Ingrese los datos de la Matriz A: ");
-            scanf("%d", &MatrizA[i][j]);
+            MatrizA[i][j] = rand() % 101;
         }
     }
-    
+    // Dimension de la matriz B
     printf("\nMATRIZ B\n");
     printf("Ingrese el numero de filas: ");
     scanf("%d", &fB);
@@ -27,53 +29,57 @@ int main(int argc, char const *argv[])
     scanf("%d", &cB);
     int MatrizB[fB][cB];
 
+    // Valores para la matriz B
     for (int i = 0; i < fB; i++)
     {
         for (int j = 0; j < cB; j++)
         {
-            printf("Ingrese los datos de la Matriz B: ");
-            scanf("%d", &MatrizB[i][j]);
+            MatrizB[i][j] = rand() % 101;
         }
     }
-     //Impresion Matriz A
-     printf("Matriz A:\n");
+    // Impresion Matriz A
+    printf("\nMatriz A:\n");
     for (int i = 0; i < fA; i++)
     {
         for (int j = 0; j < cA; j++)
         {
-            printf("%d\t", MatrizA[i][j]);
+            printf("%4d\t|", MatrizA[i][j]);
         }
-        printf("\n\n");
+        printf("\n");
     }
 
-    //Impresion Matriz B
+    // Impresion Matriz B
     printf("\nMatriz B:\n");
     for (int i = 0; i < fB; i++)
     {
         for (int j = 0; j < cB; j++)
         {
-            printf("%d\t", MatrizB[i][j]);
+            printf("%4d\t|", MatrizB[i][j]);
         }
-        printf("\n\n");
+        printf("\n");
     }
 
+    // Suma de las matrices
+    fC=fA, cC=cA;
     int MatrizC[fC][cC];
     for (int i = 0; i < fC; i++)
     {
         for (int j = 0; j < cC; j++)
         {
-            MatrizC[fC][cC]=MatrizA[fA][cA]+MatrizB[fB][cB];
-            printf("%d\t", MatrizC[i][j]);
+            MatrizC[i][j] = (MatrizA[i][j]) + (MatrizB[i][j]);
+        }
+        printf("\n");
+    }
+    // Impresion Suma de matrices
+    printf("\nSUMA\nMatriz C:\n");
+    for (int i = 0; i < fC; i++)
+    {
+        for (int j = 0; j < cC; j++)
+        {
+            printf("%4d\t|", MatrizC[i][j]);
         }
         printf("\n\n");
     }
-
-
-    
-    //printf("%d", MatrizB[fB][cB]);
-     //printf("%d", MatrizA[fA][cA]);
-
-    
 
     return 0;
 }
