3eb2b93c260ba520763c8e09699b39cd8803333e
YanouchJ <jeimy.yanouch@udla.edu.ec>
2023-11-29 15:51:14 -0500

    lineas de codigo para definir las matrices A y B e ingresar su dimension por teclado

---
+++ Ejercicio1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+
+    int fA, cA, fB, cB, fC, cC;
+    printf("MATRIZ A\n");
+    printf("Ingrese el numero de filas: ");
+    scanf("%d", &fA);
+    printf("Ingrese el numero de columnas: ");
+    scanf("%d", &cA);
+    int MatrizA[fA][cA];
+
+    for (int i = 0; i < fA; i++)
+    {
+        for (int j = 0; j < cA; j++)
+        {
+            printf("Ingrese los datos de la Matriz A: ");
+            scanf("%d", &MatrizA[i][j]);
+        }
+    }
+    
+    printf("MATRIZ B\n");
+    printf("Ingrese el numero de filas: ");
+    scanf("%d", &fB);
+    printf("Ingrese el numero de columnas: ");
+    scanf("%d", &cB);
+    int MatrizB[fB][cB];
+
+    for (int i = 0; i < fB; i++)
+    {
+        for (int j = 0; j < cB; j++)
+        {
+            printf("Ingrese los datos de la Matriz B: ");
+            scanf("%d", &MatrizB[i][j]);
+        }
+    }
+
+    return 0;
+}
